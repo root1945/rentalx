de16ecf44e0f07ab2cf172ecfbdd48cd030154fb
Victor Alencar <munternl13@gmail.com>
2021-12-12 21:21:55 -0300

    Inserindo os dados do upload no repositório

--- src/modules/cars/useCases/importCategory/ImportCategoryService.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryService.ts
@@ -42,7 +42,18 @@ class ImportCategoryService {
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
   }
 }
 
